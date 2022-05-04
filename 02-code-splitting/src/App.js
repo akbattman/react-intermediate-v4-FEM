0a074ed707daf0df3c1705609c074e43b2895e5c
Aaron Battman <aaron.k.battman@outlook.com>
2022-05-04 12:37:46 +0100

    suspense ++ at app level. lazy applied at route level && a function comp

--- 02-code-splitting/src/App.js
+++ 02-code-splitting/src/App.js
@@ -1,24 +1,31 @@
 import { render } from "react-dom";
-import SearchParams from "./SearchParams";
-import { StrictMode, useState } from "react";
+// import SearchParams from "./SearchParams"; // -- static import for code splitting
+import { StrictMode, useState, lazy, Suspense } from "react"; // lazy and suspense ++ for code splitting
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Details from "./Details";
+// import Details from "./Details"; // -- static import for code splitting
 import ThemeContext from "./ThemeContext";
+
+// Dynamic imports - imports at runtime - parcel recognises dynamic import and splits code from build time run
+// lazy returns module as promise component - lazy components applied at route level interactions
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const theme = useState("darkblue");
   return (
     <StrictMode>
       <ThemeContext.Provider value={theme}>
-        <BrowserRouter>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </BrowserRouter>
+        <Suspense fallback={<h2>loading....SUSPENSE EG:</h2>}>
+          <BrowserRouter>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
       </ThemeContext.Provider>
     </StrictMode>
   );
